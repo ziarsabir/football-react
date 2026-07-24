cde40541945082d704d9b77b159896e585ecc0ec
Ziar Sabir <ziar.sabir@gmail.com>
2026-07-24 14:26:51 +0100

    Improve refresh page scrolling

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,4 +1,3 @@
-// components/ScrollToTop.jsx
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
@@ -6,7 +5,11 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" }); 
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
   }, [pathname]);
 
   return null;
