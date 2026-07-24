744a50cd68db5431dec3f973a078e18fbd4c68e7
Ziar Sabir <ziar.sabir@gmail.com>
2026-07-24 14:38:32 +0100

    Add global scroll restoration and scroll-to-top behvaior

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,15 +1,11 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "auto",
-    });
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import {BrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+// Prevent the browser from restoring the previous scroll position 
+
+if ("scrollRestoration" in window.history) {
+  window.history.scrollRestoration = "manual"; 
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
